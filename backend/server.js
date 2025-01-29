6a4648b11d814f69837fe741327eeb8c9dc69413
Pratik-Dungano <pratikkumar.dungano119643@marwadiuniversity.ac.in>
2025-01-30 00:22:44 +0530

    changesin backend

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import useRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRouter.js'
-import orderRouter from './routes/orderRoute.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import useRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRouter.js';
+import orderRouter from './routes/orderRoute.js';
 
+// App Config
+const app = express();
+connectDB();
+connectCloudinary();
 
-//App Config
-const app=express()
-const port=process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
-//middlewares
-app.use(express.json())
-app.use(cors())
-
-//api endpoints
-app.use('/api/user',useRouter)
-app.use('/api/product',productRouter)
+// API Endpoints
+app.use('/api/user', useRouter);
+app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
-app.use('/api/order',orderRouter);
+app.use('/api/order', orderRouter);
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
-})
+app.get('/', (req, res) => {
+    res.send("API Working");
+});
 
-app.listen(port,()=>console.log('Server started on Port:'+port))
+// Export for Vercel
+export default app;
