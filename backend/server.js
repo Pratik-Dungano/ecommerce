24c10f69c1ba2f811b50326e5170d7ca527d8403
Pratik-Dungano <pratikkumar.dungano119643@marwadiuniversity.ac.in>
2025-01-30 01:10:25 +0530

    one more time db

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
     res.send("API Working");
 });
 
-// Start Server
+// Start Server (DO NOT export app)
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on Port: ${port}`));
