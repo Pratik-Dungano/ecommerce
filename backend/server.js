3ea31466069de59d3f4a90e2c42796e6508f0e48
Pratik-Dungano <pratikkumar.dungano119643@marwadiuniversity.ac.in>
2025-01-30 01:04:22 +0530

    another try in db

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,6 @@ app.get('/', (req, res) => {
     res.send("API Working");
 });
 
-// Export for Vercel
-export default app;
+// Start Server
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server started on Port: ${port}`));
